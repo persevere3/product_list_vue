114e0bf53cb33b185f65e92ec1d9dc7310d012a5
DESKTOP-T545D7R\admin <>
2023-03-06 10:08:13 +0800

    ga

--- src/components/home.vue
+++ src/components/home.vue
@@ -1447,6 +1447,7 @@ export default {
         }
 
         vm.getStore();
+        vm.getStore2();
         vm.getCategories();
         vm.getProducts('', true);
 
@@ -1480,6 +1481,79 @@ export default {
       .catch((err) => { 
         console.error(err);
         vm.login(vm.getStore);
+      });
+    },
+    appendScript(text, tag){
+      if(!text){
+        return
+      }
+
+      if(location.pathname.indexOf('shopping') > -1) {
+        return
+      }
+
+      // 
+      let script_arr = [];
+
+      let scriptItems = text.split('&lt;script');
+      scriptItems.splice(0, 1);
+
+      for(let i = 0; i < scriptItems.length; i++){
+        scriptItems[i] = '&lt;script '+ scriptItems[i].trim();
+        let attr = scriptItems[i].split('&gt;')[0];
+
+        let content = scriptItems[i].split('&gt;')[1].split("&lt;/script")[0];
+        let arr = attr.split(" ");
+        let obj = {};
+        for(let i = 0; i < arr.length; i++){
+          if(arr[i].indexOf('="') != -1){
+            obj[arr[i].split('="')[0]] = arr[i].split('="')[1].split('"')[0];
+          } 
+        }
+
+        let script = document.createElement('script');
+        for(let item in obj){
+          script.setAttribute(item, obj[item]);
+        }
+        script.textContent = content;
+
+        script_arr.push(script);
+      }
+
+      // 
+      for(let i = 0; i < script_arr.length; i++){
+        document.querySelector(tag).appendChild(script_arr[i])
+      }
+    },
+    getStore2(){
+      const vm = this;
+
+      const url = `${vm.protocol}//${vm.api}/interface/web/getStore`;
+
+      let site_webPreview = (JSON.parse(localStorage.getItem('site')).WebPreview || '') ;
+
+      let formData = new FormData();
+      formData.append("WebPreview", site_webPreview);
+
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      };
+
+      this.$http.post(url, formData, config).then((res) => {
+        if(res.data.errormessage){
+          vm.login(vm.getStore2);
+          return;
+        }
+
+        let store2 = res.data.data[0]
+        let GAText = store2.GA;
+        vm.appendScript(GAText, 'head');
+      })
+      .catch((err) => { 
+        console.error(err);
+        vm.login(vm.getStore2);
       });
     },
     getCategories() {
