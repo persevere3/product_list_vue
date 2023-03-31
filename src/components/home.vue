370969b4eebb9a9fc87265a7a87ed783c8fb2eb3
DESKTOP-T545D7R\admin <>
2023-03-31 14:17:58 +0800

    修改 getShoppingPathname()

--- src/components/home.vue
+++ src/components/home.vue
@@ -3730,11 +3730,10 @@ export default {
 
     // shopping
     getShoppingPathname(page) {
-      let shoppingHost = 'store.uniqm.com'
-
+      let shoppingHost = 'uniqm.com'
       let host = location.host;
 
-      let pageIndex = shoppingHost == host ? 1 : 0;
+      let pageIndex = host.indexOf(shoppingHost) > -1 ? 1 : 0;
 
       let pageObj = {
         index: {
